5f688609f4c01d7b60d588fec8e11c87818715d0
SamElmore625 <54898090+SamElmore625@users.noreply.github.com>
2021-03-03 00:46:20 -0500

    Started

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # This is a sample Python script.
 
-So if I add
+code = input("Please Input Code Here:\n")
+
+print ("Confirmed code:", code)
+
+print (code.replace("123562547", " "))
+#replaces the seperator with a space
+
+print(code.split)
+#splits each 3 digit code into its own element
